ffab417ab3863dabf7d97239757bac9d4e0b94a0
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-06-12 00:08:03 +0600

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'reactive-form', component: ReactiveFormComponent },
+  { path: '**', redirectTo: 'reactive-form' }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule , ReactiveFormsModule],
-  declarations: [ AppComponent, HelloComponent, ReactiveFormComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  declarations: [AppComponent, HelloComponent, ReactiveFormComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
