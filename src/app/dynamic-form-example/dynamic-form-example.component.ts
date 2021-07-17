46c28d5643f1cc6e5423c8d2b476e3a4ccabb97a
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-07-17 13:45:41 +0600

    1 file modified

--- src/app/dynamic-form-example/dynamic-form-example.component.ts
+++ src/app/dynamic-form-example/dynamic-form-example.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { of, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { QuestionBase } from './model/question-base';
-import { DropdownQuestion } from './model/question-dropdown';
-import { TextboxQuestion } from './model/question-textbox';
 import { QuestionControlService } from './service/question-control.service';
 import { QuestionService } from './service/question-control.service';
 
@@ -19,7 +17,7 @@ export class DynamicFormExampleComponent implements OnInit {
 
   constructor(
     private qcs: QuestionControlService,
-    questionService: QuestionService
+    private questionService: QuestionService
   ) {}
 
   ngOnInit() {
@@ -29,36 +27,5 @@ export class DynamicFormExampleComponent implements OnInit {
         string
       >[]);
     }
-  }
-
-  // TODO: get from a remote source of question metadata
-  getQuestions() {
-    const questions: QuestionBase<string>[] = [
-      new DropdownQuestion({
-        key: 'brave',
-        label: 'Bravery Rating',
-        options: [
-          { key: 'solid', value: 'Solid' },
-          { key: 'great', value: 'Great' },
-          { key: 'good', value: 'Good' },
-          { key: 'unproven', value: 'Unproven' }
-        ],
-        order: 3
-      }),
-      new TextboxQuestion({
-        key: 'firstName',
-        label: 'First name',
-        value: 'Bombasto',
-        required: true,
-        order: 1
-      }),
-      new TextboxQuestion({
-        key: 'emailAddress',
-        label: 'Email',
-        type: 'email',
-        order: 2
-      })
-    ];
-    return of(questions.sort((a, b) => a.order - b.order));
   }
 }
